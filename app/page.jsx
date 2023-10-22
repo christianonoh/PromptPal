3af25f903440a8f01146f9f3c3c4ec8f6658aa0f
christianonoh <onohchibyk@gmail.com>
2023-10-22 02:27:53 +0100

    Restructure app folders

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+
+const Home = () => {
+  return (
+    <section className="w-full red flex-col flex-center">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="md:hidden" />
+        <span className="orange_gradient text-center">
+          AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        PromptPal is an open-source community-driven platform for discovering and sharing AI-generated prompts.
+      </p>
+    </section>
+  )
+}
+
+export default Home
