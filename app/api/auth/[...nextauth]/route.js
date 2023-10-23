2a49faad794e5ef6ff1d820952bd44d8974e5dbe
christianonoh <onohchibyk@gmail.com>
2023-10-23 16:21:38 +0100

    Place the `signIn` and `session` fuctions into `callback` object (NextJS way)

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import connectToDB from "@utils/database";
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import connectToDB from '@utils/database';
+import User from '@models/user';
+
 
 const handler = NextAuth({
   providers: [
@@ -9,22 +11,42 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+  callbacks: {
+    async session({ session }) {
+      try {
+        // Find the user in the database by email
+        const sessionUser = await User.findOne({ email: session.user.email });
 
-      // check if the user exists in the database 
-      const userExists = true;
+        // Set the user's ID in the session
+        session.user.id = sessionUser._id.toString();
 
-      // if not, create the user in the database
-      
+        return session;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-    } catch (error) {
-      return false;
-    }
+        // Check if the user exists in the database
+        const userExists = await User.findOne({ email: profile.email });
+
+        // If the user doesn't exist, create a new user in the database
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(/[^a-zA-Z0-9]/g, '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
   },
 });
 
