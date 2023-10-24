4e2822397602ba84885ac001509cbb860802e493
christianonoh <onohchibyk@gmail.com>
2023-10-24 16:28:35 +0100

    Create PromptCard component with handleCopy functionality

---
+++ components/PromptCard.jsx
@@ -0,0 +1,66 @@
+"use client"
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { useParams, useRouter } from "next/navigation";
+
+const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete}) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(prompt.prompt);
+    setCopied(prompt.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }
+    , 3000);
+  };
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={prompt.creator.image}
+            width={40}
+            height={40}
+            alt='user_image'
+            className="rounded-full object-contain"
+          />
+        <div className="flex flex-col">
+          <h3 className="font-semibold text-gray-900">
+            {prompt.creator.username}
+          </h3>
+          <p className="text-sm text-gray-500">
+            {prompt.creator.email}
+          </p>
+        </div>
+        </div>
+
+        <div className="copy_btn">
+          <Image
+            src={copied === prompt.prompt ? "/assets/icons/tick.svg" : "/assets/icons/copy.svg"}
+            width={20}
+            height={20}
+            alt='copy_icon'
+            className="cursor-pointer"
+            onClick={handleCopyClick}
+          />
+        </div>
+      </div>
+      <p className="text-sm my-4">
+        {prompt.prompt}
+      </p>
+      <div className="flex flex-between">
+      <p className="text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(prompt.tag)}>
+        {prompt.tag}
+      </p>
+      <p className="text-sm text-gray-500">
+        {new Date(prompt.createdAt).toLocaleString('en-US', { dateStyle: 'long' })}
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default PromptCard
