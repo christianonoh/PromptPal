6fe2f80218a72a3cd294266aee4540d875085c41
christianonoh <onohchibyk@gmail.com>
2023-10-26 17:38:37 +0100

    Create handleProfileClick function and modify styling

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -19,10 +19,19 @@ const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete}) => {
     , 3000);
   };
 
+  const handleProfileClick = () => {
+    if (prompt.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${prompt.creator._id}?name=${prompt.creator.username}`);
+
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div
+          onClick={handleProfileClick}
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
             src={prompt.creator.image}
             width={40}
@@ -32,10 +41,10 @@ const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete}) => {
           />
         <div className="flex flex-col">
           <h3 className="font-semibold text-gray-900">
-            {prompt.creator.username}
+            {prompt.creator.name}
           </h3>
           <p className="text-sm text-gray-500">
-            {prompt.creator.email}
+            {prompt.creator.username}
           </p>
         </div>
         </div>
@@ -56,18 +65,18 @@ const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete}) => {
       </p>
       <div className="flex flex-between">
       <p className="text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(prompt.tag)}>
-        {prompt.tag}
+        #{prompt.tag}
       </p>
       <p className="text-sm text-gray-500">
-        {new Date(prompt.createdAt).toLocaleString('en-US', { dateStyle: 'long' })}
+        {new Date(prompt.createdAt).toLocaleString('en-US', { dateStyle: 'short' })}
       </p>
       </div>
       {session?.user.id === prompt.creator._id && pathName === '/profile' && (
         <div className="flex flex-center pt-4 gap-6 border-t">
-          <button className="black_btn" onClick={handleEdit}>
+          <button className="blue_gradient" onClick={handleEdit}>
             Edit
           </button>
-          <button className="outline_btn" onClick={handleDelete}>
+          <button className="orange_gradient" onClick={handleDelete}>
             Delete
           </button>
         </div>
