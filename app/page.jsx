500081f1ef5078ae1280c11792f8f03e3cec43e7
christianonoh <onohchibyk@gmail.com>
2023-10-26 17:32:13 +0100

    Update home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,12 @@ import Feed from "@components/Feed"
 
 const Home = () => {
   return (
-    <section className="w-full red flex-col flex-center">
+    <section className="w-full flex-col flex-center">
       <h1 className="head_text text-center">
         Discover & Share
         <br className="md:hidden" />
         <span className="orange_gradient text-center">
+          {' '}
           AI-Powered Prompts
         </span>
       </h1>
