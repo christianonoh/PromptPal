6b8f02b0cdc0fa93f08d41e0c7b2ee1e51bdd342
christianonoh <onohchibyk@gmail.com>
2023-10-26 17:35:35 +0100

    Add handleDelete and handleEdit functions for prompts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,13 +1,15 @@
 "use client"
 
 import { useSession } from "next-auth/react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import Profile from '@components/Profile'
 
 const MyProfile = () => {
   const { data: session } = useSession();
   const [prompts, setPrompts] = useState([]);
+  const router = useRouter();
+
   useEffect(() => {
     const fetchPrompts = async () => {
       try {
@@ -23,25 +25,45 @@ const MyProfile = () => {
     };
 
     if(session?.user.id) fetchPrompts();
-  }, []);
-  const handleDelete = async () => {
-    console.log('delete')
+  }, [session?.user.id]);
+
+  // Delete prompt 
+  const handleDelete = async (promptId) => {
+    const confirmDelete = confirm("Are you sure you want to delete this prompt?");
+    if(confirmDelete) {
+      try {
+        const res = await fetch(`/api/prompt/${promptId.toString()}`, {
+          method: "DELETE",
+        });
+        console.log(res);
+        if(res.ok) {
+          const filteredPrompts = prompts.filter((p) => p._id !== promptId);
+          setPrompts(filteredPrompts);
+          router.push("/profile");
+        }
+      } catch (error) {
+        throw Error(error);
+      }
+    }
   }
 
-  const handleEdit = () => {
-    console.log('edit')
+  // Edit prompt 
+  const handleEdit = (promptId) => {
+    router.push(`/update-prompt?id=${promptId}`)
   }
 
   return (
     <section>
-      <Profile
-        name="My"
-        description="This is my profile"
-        data={prompts}
-        handleDelete={handleDelete}
-        handleEdit={handleEdit}
-      />
-    </section>
+    {session?.user.id && (
+        <Profile
+          name={session.user.name.split(' ').slice(0, 2).join(' ')}
+          description="Your Creative Haven: Craft, curate, and share prompts that showcase your imaginative brilliance, sparking inspiration in others."
+          data={prompts}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+        />
+        )}    
+      </section>
   )
 }
 
